8a42bd0d04523aef07f25eccec676b4f7deb7814
plesciadc <plesciadc@me.com>
2017-02-28 14:01:53 -0500

    Initial Commit

---
+++ Travel Time WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Travel Time WatchKit Extension
+//
+//  Created by Daniel Plescia on 2/28/17.
+//  Copyright © 2017 Daniel Plescia. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
