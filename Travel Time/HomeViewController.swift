893ad304f66742f92691e37e5ea5632628c3ced4
plesciadc <plesciadc@me.com>
2017-07-21 10:40:50 -0400

    Initial Conversion to MapKit

--- Travel Time/HomeViewController.swift
+++ Travel Time/HomeViewController.swift
@@ -24,6 +24,9 @@ class HomeViewController: BaseViewController, CLLocationManagerDelegate, MKMapVi
     var centerLong = Double()
     var endLocation = CLLocationCoordinate2D()
     
+    var timer = Timer()
+    var timerRunning = false
+    
     var homeAddress = [""]
     var workAddress = [""]
     var customAddress = [""]
@@ -42,12 +45,12 @@ class HomeViewController: BaseViewController, CLLocationManagerDelegate, MKMapVi
         self.mapView.showsUserLocation = false
         self.addSlideMenuButton()
         self.addMapsMenuButton()
+        self.convertAddress()
         self.navigationItem.rightBarButtonItem?.isEnabled = false
         mapView.isUserInteractionEnabled = false
         // Do any additional setup after loading the view, typically from a nib.
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
-        locationManager.requestLocation()
     }
     
     override func didReceiveMemoryWarning() {
@@ -56,13 +59,18 @@ class HomeViewController: BaseViewController, CLLocationManagerDelegate, MKMapVi
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        currentLocation = locations[0]
-        if gotInitialLocation == false {
-        getTime()
-        gotInitialLocation = true
+        if locations[0].coordinate.latitude != currentLocation.coordinate.latitude {
+            currentLocation = locations[0]
+            testGetTime()
+            if gotInitialLocation == false {
+                mapView.setCenter(currentLocation.coordinate, animated: true)
+                gotInitialLocation = true
+            }
+        } else {
+            if timerRunning == false {
+            runTimer()
+            }
         }
-        mapView.setCenter(currentLocation.coordinate, animated: true)
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -75,24 +83,105 @@ class HomeViewController: BaseViewController, CLLocationManagerDelegate, MKMapVi
         print(error.localizedDescription)
     }
     
+    func convertAddress() {
+        let addressArray = UserDefaults.standard.value(forKey: "homeAddress") as! Array<String>
+        let rawAddress = addressArray[0]
+        globalAddress = rawAddress
+
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(rawAddress) { (placemarks, error) in
+            guard
+                let placemarks = placemarks,
+                let destinationPlacemark = placemarks.first?.location
+                else {
+                    // handle no location found
+                    return
+            }
+            
+            self.endLocation = destinationPlacemark.coordinate
+            self.locationManager.requestLocation()
+        }
+    }
+    
     func updateUI(success: Bool) {
         self.spinner.stopAnimating()
         self.greyBG.isHidden = true
         self.mapView.isUserInteractionEnabled = true
         self.mapView.showsUserLocation = true
         self.navigationItem.rightBarButtonItem?.isEnabled = true
-        if success == true {
-        let mapSpan = MKCoordinateSpanMake((deltaLat + 0.012), (deltaLong + 0.012))
-        let centerCoordinate = CLLocationCoordinate2DMake(centerLat, centerLong)
-        let coordinateRegion = MKCoordinateRegionMake(centerCoordinate, mapSpan)
-        //let startingPoint = Pins(title: "Starting Location", coordinate: currentLocation.coordinate)
-        let endingPoint = Pins(title: "Ending Location", coordinate: endLocation)
-        //self.mapView.addAnnotation(startingPoint)
-        self.mapView.addAnnotation(endingPoint)
-        self.mapView.setRegion(coordinateRegion, animated: true)
-        }
+    }
+    // New method for refreshing location every X seconds
+    func startRefresh() {
+        timerRunning = false
+        locationManager.requestLocation()
         
     }
+    // Method to run timer. Time interval to be replaced with user choice for updating
+    func runTimer() {
+        timerRunning = true
+        timer = Timer.scheduledTimer(timeInterval: 5, target: self,   selector: (#selector(self.startRefresh)), userInfo: nil, repeats: false)
+    }
+    
+    
+    func testGetTime() {
+        let sourceLocation = CLLocationCoordinate2D(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude)
+        let sourcePlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
+        let destinationPlacemark = MKPlacemark(coordinate: endLocation, addressDictionary: nil)
+        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+
+        let destinationAnnotation = MKPointAnnotation()
+        destinationAnnotation.title = "Ending Location"
+        if let location = destinationPlacemark.location {
+            destinationAnnotation.coordinate = location.coordinate
+        }
+        
+        self.mapView.showAnnotations([destinationAnnotation], animated: true )
+
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.source = sourceMapItem
+        directionRequest.destination = destinationMapItem
+        directionRequest.transportType = .automobile
+        directionRequest.requestsAlternateRoutes = true
+        
+        // Calculate the direction
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate {
+            (response, error) -> Void in
+            
+            guard let response = response else {
+                if let error = error {
+                    print("Error: \(error)")
+                }
+                
+                return
+            }
+            for route in response.routes {
+            self.mapView.add((route.polyline), level: MKOverlayLevel.aboveRoads)
+            var summaryText = ""
+            let routeTime = route.expectedTravelTime / 60
+            summaryText.append(routeTime + " minutes via " + route.name + " (" + route.distance + ")")
+            }
+            
+            let rect = response.routes[0].polyline.boundingMapRect
+            let bound = self.mapView.mapRectThatFits(rect, edgePadding: UIEdgeInsetsMake(90, 35, 35, 50))
+            self.mapView.setRegion(MKCoordinateRegionForMapRect(bound), animated: false)
+            self.updateUI(success: true)
+            if self.timerRunning == false {
+            self.runTimer()
+            }
+        }
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 3.0
+        
+        return renderer
+    }
+    
     
     func getTime() {
         let addressArray = UserDefaults.standard.value(forKey: "homeAddress") as! Array<String>
@@ -198,6 +287,7 @@ class HomeViewController: BaseViewController, CLLocationManagerDelegate, MKMapVi
                 DispatchQueue.main.async {
                 self.directionsLabel.text = summaryText
                 self.updateUI(success: true)
+                self.runTimer()
                 }
             }
         }
